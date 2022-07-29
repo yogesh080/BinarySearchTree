e2430122717c64620133152122a51ef2aaeac4d7
Yogesh <yogeshbietjhs@gmail.com>
2022-07-29 19:48:51 +0530

    Binary search tree and delete from tree

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            this.value=value;
+            
+        }
+    }
+    class BinarySearchTree
+    {
+        public static Node Search(Node root, int value)
+        {
+            if (root.value==value)
+            {
+                return root;
+            }
+            else if(value < root.value)
+            {
+                root=Search(root.left, value);
+            }
+            else if(value > root.value)
+            {
+                root = Search(root.right, value);
+            }
+            return root;
+        }
+
+        public static Node Insert(Node root, int value)
+        {
+            if (root.value == value)
+            {
+                return root;
+            }
+            else if (value < root.value)
+            {
+                if (root.left != null)
+                {
+                    root.left = Insert(root.left, value);
+                }
+                else
+                {
+                    Node newNode = new Node(value);
+                    root.left = newNode;
+
+                }
+            }
+            else if (value < root.value)
+            {
+                if (root.right != null)
+                {
+                    root.right = Insert(root.right, value);
+                }
+                else
+                {
+                    Node newNode = new Node(value);
+                    root.right = newNode;
+
+                }
+            }
+            return root;
+        }
+    }
+   
+    
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+            Node root = new Node(30);
+            Node n1 = new Node(15);
+            Node n2 = new Node(45);
+            Node n3 = new Node(6);
+            Node n4 = new Node(41);
+            Node n5 = new Node(54);
+
+
+
+            root.left = n1;
+            root.right = n2;
+            n1.right = n3;
+            n2.left = n4;
+            n3.right = n5;
+
+
+            Node nodeToFind = BinarySearchTree.Search(root, 41);
+            //Console.WriteLine(nodeToFind.value);
+
+            Node newRoot = BinarySearchTree.Insert(root, 85);
+            Console.WriteLine(newRoot.right.right.right.value);
+
+
+
+        }
+    }
+}
